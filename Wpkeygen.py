bd8dd9d722fad2619acd7b3e050eedaf4b3651b0
Fredric <mr@iamfredric.com>
2014-04-19 20:22:22 +0200

    Fix for ST3
    Now working with sublime text 3

--- Wpkeygen.py
+++ Wpkeygen.py
@@ -2,13 +2,14 @@ import sublime, sublime_plugin, hashlib, random
 
 class WpkeygenCommand(sublime_plugin.TextCommand):
 
-    def run(self, edit):
-    	ListReplacements = []
-    	SecondRound = []
-    	Hasher = hashlib.sha1()
-      	RegionsResult = self.view.find_all(r"(define\(')([A-Z_]+)(_)(KEY|SALT)(',)(.*?)(\);)", sublime.IGNORECASE, "\\1\\2\\3\\4\\5'KUKEN'\\7", ListReplacements)
-      	for i, thisregion in reversed(list(enumerate(RegionsResult))):
-			hashstring = "%032x" %  random.getrandbits(128)
-			Hasher.update(hashstring)
-			newsalt = ListReplacements[i].replace('KUKEN', Hasher.hexdigest())
-			self.view.replace(edit, thisregion, newsalt)
+  def run(self, edit):
+    ListReplacements = []
+    Hasher = hashlib.sha1()
+
+    RegionsResult = self.view.find_all(r"(define\(')([A-Z_]+)(_)(KEY|SALT)(',)(.*?)(\);)", sublime.IGNORECASE, "\\1\\2\\3\\4\\5'THISBITSHOLDBEREPLACED'\\7", ListReplacements)
+
+    for i, thisregion in reversed(list(enumerate(RegionsResult))):
+      hashstring = "%032x" %  random.getrandbits(128)
+      Hasher.update(hashstring.encode('utf-8'))
+      newsalt = ListReplacements[i].replace('THISBITSHOLDBEREPLACED', Hasher.hexdigest())
+      self.view.replace(edit, thisregion, newsalt)
